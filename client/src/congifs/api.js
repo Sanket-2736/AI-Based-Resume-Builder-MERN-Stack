6c449137b60dc8dc60ee85f48091156b89fe505e
Sanket-2736 <sanketbelekar29@gmail.com>
2026-01-15 16:00:27 +0530

    update API base URL to production server

--- client/src/congifs/api.js
+++ client/src/congifs/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://ai-based-resume-builder-mern-stack.onrender.com'
 });
 
 export default api;
